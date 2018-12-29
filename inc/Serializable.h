2a6fece8fbdc3c2654dad7bea66bf42d1bf2ba36
NolkMan <4n931mail@gmail.com>
2018-12-29 19:44:39 +0100

    Serialization framework may now be working
    It compiles
    But it was not yet tested, as unit tests are being made

--- inc/Serializable.h
+++ inc/Serializable.h
@@ -1,24 +1,73 @@
 #ifndef __BOTME_INC_SERIALIZABLE_H
 #define __BOTME_INC_SERIALIZABLE_H
 
-class Serializable{
-protected:
-	Serializable();
+#include <vector>
+#include <queue>
 
-	void serializeDouble(double);
-	void serializeInt(int);
-	void serializeUInt(unsigned int);
-	void addSerialiable(Serializable *);
+class SerializationObject{
+	std::queue<unsigned int> data;
+	std::queue<SerializationObject> objects;
+public:
+	SerializationObject();
+	SerializationObject(const std::vector<unsigned int>&);
+	SerializationObject(const std::vector<unsigned int>& , int &pos);
+
+	std::vector<unsigned int> getData();
+
+	void putDouble(double);
+	void putInt(int);
+	void putUInt(unsigned int);
+	void putObject(SerializationObject);
 
 	double getDouble();
 	int getInt();
 	unsigned int getUInt();
-	Serializable* getSerializable();
+	SerializationObject getObject();
+};
+
+class Serializable{
+protected:
+	Serializable();
 
 public:
-	virtual void serialize() = 0;
-	virtual void deserialize() = 0;
+	virtual SerializationObject serialize() = 0;
 
+};
+
+class UnitTestSerialization : public Serializable{
+	const static double d1 = 0.2345677771;
+	const static double d2 = 0.11235623423;
+	const static int i1 = -1000000000;
+	const static auto o1 = UnitTestSerialization(0);
+	const static auto o2 = UnitTestSerialization(1);
+	const static auto o3 = UnitTestSerialization(2);
+	int dataType;
+public:
+	UnitTestSerialization(int dataType):dataType(dataType){}
+	UnitTestSerialization(const SerializationObject&);
+	SerializationObject serialize(){
+		SerializationObject so;
+		if (dataType == 0){
+			so.putDouble(d1);
+		}else if (dataType == 1){
+			so.putDouble(d1);
+			so.putInt(i1);
+			so.putDouble(d2);
+		}else if (dataType == 2){
+			so.putDouble(d1);
+			so.putInt(i1);
+			so.putObject(o1.serialize());
+			so.putObject(o2.serialize());
+		}else{ 
+			so.putDouble(d2);
+			so.putDouble(d1);
+			so.putInt(i1);
+			so.putObject(o3.serialize());
+			so.putObject(o2.serialize());
+			so.putObject(o3.serialize());
+		}
+		return so;
+	}
 };
 
 #endif
--- src/Serializable.cpp
+++ src/Serializable.cpp
@@ -1,8 +1,97 @@
 #include "Serializable.h"
 
-void serializeDouble(double d){
+SerializationObject::SerializationObject(){
+}
+
+SerializationObject::SerializationObject(const std::vector<unsigned int>& v){
+	int pos = 0;
+	int next = v[pos];
+	for (int i = pos + 1 ; i < pos + 1 + next; i++)
+		data.push(v[i]);
+	
+	pos += next + 1; // pos is at amount of objects
+	int objs = v[pos];
+	pos++;
+	for (int i = 0; i < objs ; i++){
+		objects.push(SerializationObject(v, pos));
+	}
+}
+
+SerializationObject::SerializationObject(const std::vector<unsigned int>& v, int &pos){
+	// Warning: this code was copy pasted a little up
+	int next = v[pos];
+	for (int i = pos + 1 ; i < pos + 1 + next; i++)
+		data.push(v[i]);
+	
+	pos += next + 1; // pos is at amount of objects
+	int objs = v[pos];
+	pos++;
+	for (int i = 0; i < objs ; i++){
+		objects.push(SerializationObject(v, pos));
+	}
+}
+
+std::vector<unsigned int> SerializationObject::getData(){
+	std::vector<unsigned int> v;
+	v.push_back(data.size());
+	while (!data.empty()){
+		v.push_back(data.front());
+		data.pop();
+	}
+	v.push_back(objects.size());
+	while (!objects.empty()){
+		for (const auto& ui : objects.front().getData())
+			v.push_back(ui);
+		objects.pop();
+	}
+	return v;
+}
+
+void SerializationObject::putDouble(double d){
 	union { unsigned long long i; double d; } u;
 	u.d = d;
 	unsigned int st = u.i >> 32;
 	unsigned int nd = (u.i << 32) >> 32;
+	data.push(st);
+	data.push(nd);
 }
+
+double SerializationObject::getDouble(){
+	union { unsigned long long ull; double d;} u;
+	unsigned long long st = data.front(); data.pop();
+	st <<=  static_cast<unsigned long long>(32);
+	unsigned long long nd = data.front(); data.pop();
+	u.ull = st + nd;
+	return u.d;
+}
+
+void SerializationObject::putInt(int i){
+	union { unsigned int ui; int i; } u;
+	u.i = i;
+	data.push(u.ui);
+}
+
+int SerializationObject::getInt(){
+	union { unsigned int ui; int i;} u;
+	u.ui = data.front(); data.pop();
+	return u.i;
+}
+
+void SerializationObject::putUInt(unsigned int ui){
+	data.push(ui);
+}
+
+unsigned int SerializationObject::getUInt(){
+	auto ui = data.front(); data.pop();
+	return ui;
+}
+
+void SerializationObject::putObject(SerializationObject o){
+	objects.push(o);
+}
+
+SerializationObject SerializationObject::getObject(){
+	auto o = objects.front(); objects.pop();
+	return o;
+}
+
